69261351f51aad7fd41044864704422f03d0b25f
kittipongnuanyai <kittipong.nuanyai@gmail.com>
2022-11-05 12:12:15 +0700

    finish splash screen

---
+++ lib/views/splash_ui.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:ice_assets/views/home_ui.dart';
+
+
+class SplashUI extends StatefulWidget {
+  const SplashUI({Key? key}) : super(key: key);
+
+  @override
+  State<SplashUI> createState() => _SplashUIState();
+}
+
+class _SplashUIState extends State<SplashUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
